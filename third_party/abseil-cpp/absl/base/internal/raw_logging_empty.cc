4c1182a7436553c17d3cc42fa980e5211a068786
weolar <weolar@qq.com>
2025-12-17 12:34:50 +0800

    * 提交缺失的文件

---
+++ third_party/abseil-cpp/absl/base/internal/raw_logging_empty.cc
@@ -0,0 +1,32 @@
+﻿// Copyright 2017 The Abseil Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "absl/base/internal/raw_logging.h"
+
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+#include "absl/base/attributes.h"
+#include "absl/base/config.h"
+#include "absl/base/internal/atomic_hook.h"
+#include "absl/base/internal/errno_saver.h"
+#include "absl/base/log_severity.h"
+
+void absl::raw_log_internal::RawLog(enum absl::LogSeverity, char const*, int, char const*, ...)
+{
+}
